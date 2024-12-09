2a746936ce778300ef9111b1c6e3382c794e2ce6
Marcos Sousa <marcosp.sousa@gmail.com>
2024-12-09 10:10:05 -0300

    Finished search input

---
+++ app/src/main/java/br/com/coupledev/bookpedia/book/presentation/book_list/components/BookSearchBar.kt
@@ -0,0 +1,120 @@
+package br.com.coupledev.bookpedia.book.presentation.book_list.components
+
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.foundation.background
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.text.selection.LocalTextSelectionColors
+import androidx.compose.foundation.text.selection.TextSelectionColors
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.OutlinedTextFieldDefaults
+import androidx.compose.material3.Text
+import androidx.compose.material3.minimumInteractiveComponentSize
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.tooling.preview.PreviewLightDark
+import br.com.coupledev.bookpedia.R
+import br.com.coupledev.bookpedia.ui.theme.BookpediaTheme
+import br.com.coupledev.bookpedia.ui.theme.DarkBlue
+import br.com.coupledev.bookpedia.ui.theme.DesertWhite
+import br.com.coupledev.bookpedia.ui.theme.SandYellow
+
+@Composable
+fun BookSearchBar(
+    searchQuery: String,
+    onSearchQueryChange: (String) -> Unit,
+    onItemSearch: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    CompositionLocalProvider(
+        LocalTextSelectionColors provides TextSelectionColors(
+            handleColor = SandYellow,
+            backgroundColor = SandYellow
+        )
+    ) {
+
+        OutlinedTextField(
+            value = searchQuery,
+            onValueChange = onSearchQueryChange,
+            shape = RoundedCornerShape(100),
+            colors = OutlinedTextFieldDefaults.colors(
+                cursorColor = DarkBlue,
+                focusedBorderColor = SandYellow,
+            ),
+            placeholder = {
+                Text(
+                    text = stringResource(R.string.search_hint)
+                )
+            },
+            leadingIcon = {
+                Icon(
+                    imageVector = Icons.Default.Search,
+                    contentDescription = null,
+                    tint = MaterialTheme.colorScheme.onSurface
+                )
+            },
+            singleLine = true,
+            keyboardActions = KeyboardActions(
+                onSearch = {
+                    onItemSearch()
+                }
+            ),
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Text,
+                imeAction = ImeAction.Search
+            ),
+            trailingIcon = {
+                AnimatedVisibility(
+                    visible = searchQuery.isNotBlank()
+                ) {
+                    IconButton(
+                        onClick = {
+                            onSearchQueryChange("")
+                        }
+                    ) {
+                        Icon(
+                            imageVector = Icons.Default.Close,
+                            contentDescription = stringResource(R.string.close_hint),
+                            tint = MaterialTheme.colorScheme.onSurface
+                        )
+                    }
+                }
+            },
+            modifier = modifier
+                .background(
+                    shape = RoundedCornerShape(100),
+                    color = if (isSystemInDarkTheme()) {
+                        DarkBlue
+                    } else DesertWhite,
+                )
+                .minimumInteractiveComponentSize()
+        )
+    }
+}
+
+@PreviewLightDark
+@Composable
+private fun BookSearchBarPreview() {
+    BookpediaTheme {
+        BookSearchBar(
+            searchQuery = "Kotlin",
+            onItemSearch = {},
+            onSearchQueryChange = {},
+            modifier = Modifier
+                .fillMaxWidth()
+        )
+    }
+}
--- app/src/main/java/br/com/coupledev/bookpedia/ui/theme/Color.kt
+++ app/src/main/java/br/com/coupledev/bookpedia/ui/theme/Color.kt
@@ -9,3 +9,8 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val DarkBlue = Color(0xFF0B405E)
+val DesertWhite = Color(0xFFF7F7F7)
+val SandYellow = Color(0xFFFFBD64)
+val LightBlue = Color(0xFF9AD9FF)
